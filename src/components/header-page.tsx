b10fe56653eb0ed8cf3fee0a4099a54ae861acfb
Dickson Wu <wbs.dickson@gmail.com>
2026-01-12 20:15:28 +0800

    feat: Update HeaderPage component to accept containerClassName prop and enhance styling

--- src/components/header-page.tsx
+++ src/components/header-page.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { toCapitalized } from "@/lib/utils";
+import { cn, toCapitalized } from "@/lib/utils";
 
 export default function HeaderPage({
   children,
   title,
   pageActions,
   capitalizeTitle = true,
+  containerClassName,
 }: {
   children: React.ReactNode;
   title: string;
   pageActions?: React.ReactNode;
   capitalizeTitle?: boolean;
+  containerClassName?: string;
 }) {
   return (
-    <div className="space-y-4">
+    <div className={cn("space-y-4", containerClassName)}>
       <div className="flex w-full items-center justify-between">
         <h2 className="text-3xl font-bold tracking-tight">
           {capitalizeTitle ? toCapitalized(title) : title}
