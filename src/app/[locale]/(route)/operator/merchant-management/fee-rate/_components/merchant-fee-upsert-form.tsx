a14db264dd2f69f62414930e3bbce5c086a6093d
Dickson Wu <wbs.dickson@gmail.com>
2026-01-14 10:18:44 +0800

    fix build

--- src/app/[locale]/(route)/operator/merchant-management/fee-rate/_components/merchant-fee-upsert-form.tsx
+++ src/app/[locale]/(route)/operator/merchant-management/fee-rate/_components/merchant-fee-upsert-form.tsx
@@ -262,12 +262,7 @@ export default function MerchantFeeUpsertForm({
 
         <div className="flex justify-end gap-2">
           {onCancel && (
-            <Button
-              
-              variant="outline"
-              className="h-9"
-              onClick={onCancel}
-            >
+            <Button variant="outline" className="h-9" onClick={onCancel}>
               {t("buttons.cancel")}
             </Button>
           )}
