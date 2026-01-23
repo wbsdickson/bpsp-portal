01c02cf8db7879525cbcdaac2af57229b3537bcd
Dickson Wu <wbs.dickson@gmail.com>
2026-01-23 21:11:44 +0800

    add agent skills

--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by git hooks before commits
 // Last update timestamp in JST (Japan Standard Time)
-export const LAST_UPDATE = "2026-01-23 21:57:30 JST";
+export const LAST_UPDATE = "2026-01-23 22:11:44 JST";
