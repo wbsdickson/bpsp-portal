0217d9f60e51bc342a1ee70a95589986e50d146e
hoangnhat1997 <phamvuonghoangnhat@gmail.com>
2026-01-23 09:29:38 +0700

    Add locale switcher trigger in header

--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by git hooks before commits
 // Last update timestamp in JST (Japan Standard Time)
-export const LAST_UPDATE = "2026-01-22 22:09:06 JST";
+export const LAST_UPDATE = "2026-01-23 11:29:38 JST";
