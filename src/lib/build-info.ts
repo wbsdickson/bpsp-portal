c3b5fcdf76fd43d06731847c93c311d9385e7325
hoangnhat1997 <phamvuonghoangnhat@gmail.com>
2026-01-23 10:09:55 +0700

    Add USA flag

--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by git hooks before commits
 // Last update timestamp in JST (Japan Standard Time)
-export const LAST_UPDATE = "2026-01-23 11:29:38 JST";
+export const LAST_UPDATE = "2026-01-23 12:09:55 JST";
