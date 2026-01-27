adf19c1176fc4638b21ca6cd28a9430027be7142
Dickson Wu <wbs.dickson@gmail.com>
2026-01-27 09:45:28 +0800

    fix prop

--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by git hooks before commits
 // Last update timestamp in JST (Japan Standard Time)
-export const LAST_UPDATE = "2026-01-27 10:43:30 JST";
+export const LAST_UPDATE = "2026-01-27 10:45:28 JST";
