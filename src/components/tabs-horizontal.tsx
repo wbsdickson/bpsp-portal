5ae298b361038bda9e8a144be642c0094d0c7306
Dickson Wu <wbs.dickson@gmail.com>
2026-01-29 10:41:39 +0800

    fix build

--- src/components/tabs-horizontal.tsx
+++ src/components/tabs-horizontal.tsx
@@ -150,7 +150,7 @@ const TabsHorizontal = ({
               </span>
             );
           })}
-        </>
+        </TabsList>
       </div>
     </div>
   );
--- src/lib/build-info.ts
+++ src/lib/build-info.ts
@@ -1,3 +1,3 @@
 // This file is automatically updated by git hooks before commits
 // Last update timestamp in JST (Japan Standard Time)
-export const LAST_UPDATE = "2026-01-29 11:40:11 JST";
+export const LAST_UPDATE = "2026-01-29 11:41:40 JST";
